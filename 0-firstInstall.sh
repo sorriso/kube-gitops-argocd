7fd50e93848034482ac95b4352cda5332e9c421f
sorriso <sorriso.tls@gmail.com>
2022-10-19 10:10:31 +0200

    #3

--- 0-firstInstall.sh
+++ 0-firstInstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #nerdctl -n k8s.io pull nginx:1.23.1-alpine
-kubectl create namespace longhorn-system
-kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.3.2/deploy/longhorn.yaml
+kubectl apply -f common
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo '----------'
+echo $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo '----------'
+kubectl apply -f ingress
